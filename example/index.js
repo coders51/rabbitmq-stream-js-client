07ac9c060af52b1884c4d945f974e1afa4aedb17
GPad <gpadovani@gmail.com>
2022-12-28 11:08:24 +0100

    Create and example for publishing on stream

---
+++ example/index.js
@@ -0,0 +1,68 @@
+const rabbit = require("rabbitmq-stream-js-client")
+const amqplib = require("amqplib")
+const { randomUUID } = require("crypto")
+
+async function main() {
+  const streamName = `example-${randomUUID()}`
+  console.log(`Create stream ${streamName}`)
+
+  const connection = await rabbit.connect({
+    hostname: "localhost",
+    port: 5552,
+    username: "rabbit",
+    password: "rabbit",
+    vhost: "/",
+    heartbeat: 0,
+  })
+  await connection.createStream({ stream: streamName, arguments: {} })
+  const producer = await connection.declarePublisher({ stream: streamName })
+
+  await producer.send(BigInt(1), "ciao")
+
+  await createClassicConsumer(streamName)
+
+  await connection.deleteStream({ stream: streamName })
+
+  await connection.close()
+}
+
+/**
+ *
+ * @param {string} queueName name of the stream to consume
+ * @returns {Promise<void>}
+ */
+async function createClassicConsumer(queueName) {
+  return new Promise(async (res, rej) => {
+    try {
+      const conn = await amqplib.connect("amqp://rabbit:rabbit@localhost")
+      const ch = await conn.createChannel()
+      await ch.prefetch(1000)
+      await ch.consume(
+        queueName,
+        async (msg) => {
+          if (!msg) {
+            return
+          }
+
+          console.log("ACK", msg.content.toString())
+          ch.ack(msg)
+          if (msg.content.toString() === "ciao") {
+            await ch.close()
+            await conn.close()
+            res()
+          }
+        },
+        { arguments: { "x-stream-offset": "first" } }
+      )
+    } catch (err) {
+      rej(err)
+    }
+  })
+}
+
+main()
+  .then(() => console.log("done!"))
+  .catch((res) => {
+    console.log("ERROR ", res)
+    process.exit(-1)
+  })
