1adfbda91a490f58ee9788d5948882d7e0fd76d1
Alberto Barrilá <alberto.barrila@gmail.com>
2023-01-10 10:13:28 +0100

    add subscribe command

--- src/requests/data_writer.ts
+++ src/requests/data_writer.ts
@@ -4,8 +4,11 @@ export interface DataWriter {
   writeUInt16(data: number): void
   writeUInt32(data: number): void
   writeUInt64(data: bigint): void
+<<<<<<< HEAD
   writeInt32(length: number): void
   writeData(data: string | Buffer): void
   writeString(data: string): void
+=======
+>>>>>>> c56dfb3 (add subscribe command)
   writeInt64(data: bigint): void
 }
