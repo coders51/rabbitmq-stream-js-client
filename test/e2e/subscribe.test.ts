b8a468110cc871316675b7042c36828bb1dacd75
Alberto Barrilá <alberto.barrila@gmail.com>
2023-01-10 10:13:28 +0100

    add subscribe command

--- test/e2e/subscribe.test.ts
+++ test/e2e/subscribe.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai"
-import { connect } from "../../src/connection"
+import { connect, Connection } from "../../src/connection"
 import { Offset } from "../../src/requests/subscribe_request"
 import { Rabbit } from "../support/rabbit"
 import { eventually } from "../support/util"
@@ -7,26 +7,22 @@ import { eventually } from "../support/util"
 describe("subscribe", async () => {
   const rabbit = new Rabbit()
   const streamName = "test-stream"
+  let connection: Connection
 
-  beforeEach(async () => {
+  before(async () => {
     await rabbit.createStream(streamName)
-  })
-
-  afterEach(async () => {
-    await rabbit.deleteStream(streamName)
-  })
-
-  it("subscribe to next message", async () => {
-    const connection = await connect({
+    connection = await connect({
       hostname: "localhost",
       port: 5552,
       username: "rabbit",
       password: "rabbit",
       vhost: "/",
-      frameMax: 0,
-      heartbeat: 0,
+      frameMax: 0, // not used
+      heartbeat: 0, // not used
     })
+  })
 
+  it("subscribe to next message", async () => {
     const res = await connection.subscribe({
       subscriptionId: 1,
       stream: streamName,
@@ -37,20 +33,9 @@ describe("subscribe", async () => {
     await eventually(async () => {
       expect(res.ok).eql(true)
     }, 5000)
-    await connection.close()
-  }).timeout(10000)
+  })
 
   it("subscribe to first message", async () => {
-    const connection = await connect({
-      hostname: "localhost",
-      port: 5552,
-      username: "rabbit",
-      password: "rabbit",
-      vhost: "/",
-      frameMax: 0,
-      heartbeat: 0,
-    })
-
     const res = await connection.subscribe({
       subscriptionId: 2,
       stream: streamName,
@@ -61,20 +46,9 @@ describe("subscribe", async () => {
     await eventually(async () => {
       expect(res.ok).eql(true)
     }, 5000)
-    await connection.close()
-  }).timeout(10000)
+  })
 
   it("subscribe to last message", async () => {
-    const connection = await connect({
-      hostname: "localhost",
-      port: 5552,
-      username: "rabbit",
-      password: "rabbit",
-      vhost: "/",
-      frameMax: 0,
-      heartbeat: 0,
-    })
-
     const res = await connection.subscribe({
       subscriptionId: 3,
       stream: streamName,
@@ -85,20 +59,9 @@ describe("subscribe", async () => {
     await eventually(async () => {
       expect(res.ok).eql(true)
     }, 5000)
-    await connection.close()
-  }).timeout(10000)
+  })
 
   it("subscribe to offset message", async () => {
-    const connection = await connect({
-      hostname: "localhost",
-      port: 5552,
-      username: "rabbit",
-      password: "rabbit",
-      vhost: "/",
-      frameMax: 0,
-      heartbeat: 0,
-    })
-
     const res = await connection.subscribe({
       subscriptionId: 4,
       stream: streamName,
@@ -109,20 +72,9 @@ describe("subscribe", async () => {
     await eventually(async () => {
       expect(res.ok).eql(true)
     }, 5000)
-    await connection.close()
-  }).timeout(10000)
+  })
 
   it("subscribe to date message", async () => {
-    const connection = await connect({
-      hostname: "localhost",
-      port: 5552,
-      username: "rabbit",
-      password: "rabbit",
-      vhost: "/",
-      frameMax: 0,
-      heartbeat: 0,
-    })
-
     const res = await connection.subscribe({
       subscriptionId: 5,
       stream: streamName,
@@ -133,6 +85,5 @@ describe("subscribe", async () => {
     await eventually(async () => {
       expect(res.ok).eql(true)
     }, 5000)
-    await connection.close()
-  }).timeout(10000)
+  })
 })
