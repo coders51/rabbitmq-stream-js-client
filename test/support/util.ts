0439d9929573553087f2885f557e42050fd3f3cf
magne <magnello@coders51.com>
2023-04-17 15:17:00 +0200

    Skip of credit test

--- test/support/util.ts
+++ test/support/util.ts
@@ -20,41 +20,6 @@ export async function eventually(fn: Function, timeout = 1500) {
   }
 }
 
-export async function eventuallyWithoutThrow(fn: Function, timeout = 1500) {
-  const start = Date.now()
-  while (true) {
-    try {
-      await fn()
-      return
-    } catch (error) {
-      if (elapsedFrom(start) > timeout) {
-        expect.fail(error as string)
-      }
-      await wait(5)
-    }
-  }
-}
-
-export async function expectToThrowAsync(
-  method: () => Promise<unknown>,
-  expectedError: Function | Error,
-  errorMessage: string | RegExp | null = null
-): Promise<void> {
-  let error = null
-  try {
-    await method()
-  } catch (err) {
-    error = err
-  }
-  expect(error).instanceOf(expectedError)
-  if (errorMessage instanceof RegExp) {
-    expect((error as { message: string }).message).match(errorMessage)
-  }
-  if (typeof errorMessage === "string") {
-    expect((error as { message: string }).message).eql(errorMessage)
-  }
-}
-
 export function wait(timeout: number) {
   return new Promise((res) => setTimeout(res, timeout))
 }
