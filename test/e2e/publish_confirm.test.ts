948114f17446d2984786a28d21b3b8a9737f9a02
GPad <gpadovani@gmail.com>
2023-05-17 11:46:18 +0200

    Semplify some test

--- test/e2e/publish_confirm.test.ts
+++ test/e2e/publish_confirm.test.ts
@@ -8,16 +8,14 @@ import { createConnection } from "../support/fake_data"
 describe("publish a message and get confirmation", () => {
   const rabbit = new Rabbit(username, password)
   let connection: Connection
-  let confirmed: boolean
   let stream: string
-  const publishResponses: bigint[][] = []
+  const publishResponses: { error: number | null; ids: bigint[] }[] = []
   const publisherRef = "my publisher"
 
   beforeEach(async () => {
     connection = await createConnection(username, password)
     stream = `my-stream-${randomUUID()}`
     await rabbit.createStream(stream)
-    confirmed = false
     publishResponses.splice(0)
   })
   afterEach(() => connection.close())
@@ -26,59 +24,34 @@ describe("publish a message and get confirmation", () => {
   it("after the server replies with a confirm, the confirm callback is invoked", async () => {
     const publisher = await connection.declarePublisher({ stream, publisherRef })
     const publishingId = 1n
-    publisher.on("publish_confirm", (err: number | null, pubIds: bigint[]) => {
-      confirmed = !err
-      publishResponses.push(pubIds)
-    })
+    publisher.on("publish_confirm", (error, ids) => publishResponses.push({ error, ids }))
 
     await publisher.send(publishingId, Buffer.from(`test${randomUUID()}`))
 
     await eventually(async () => {
       expect((await rabbit.getQueueInfo(stream)).messages).eql(1)
-      expect(publishResponses[0]).to.include(1n)
-      expect(confirmed).true
+      expect(publishResponses).eql([{ error: null, ids: [publishingId] }])
     }, 5000)
   }).timeout(10000)
 
   it("after the server replies with a confirm, the confirm callback is invoked with the publishingId as an argument", async () => {
-    let publishingIds: bigint[] = []
     const publisher = await connection.declarePublisher({ stream, publisherRef })
-    publisher.on("publish_confirm", (_err: number | null, pubIds: bigint[]) => (publishingIds = pubIds))
+    publisher.on("publish_confirm", (error, ids) => publishResponses.push({ error, ids }))
 
     await publisher.send(1n, Buffer.from(`test${randomUUID()}`))
 
-    await eventually(async () => {
-      expect((await rabbit.getQueueInfo(stream)).messages).eql(1)
-    }, 10000)
+    await eventually(async () => expect((await rabbit.getQueueInfo(stream)).messages).eql(1), 10000)
     const lastPublishingId = await publisher.getLastPublishingId()
-    expect(publishingIds.pop()).equals(lastPublishingId)
+    expect(publishResponses).eql([{ error: null, ids: [lastPublishingId] }])
   }).timeout(10000)
 
-  it("after the server replies with an error, the error callback is invoked", async () => {
-    const errors: number[] = []
+  it("after the server replies with an error, the error callback is invoked with an error", async () => {
     const publisher = await connection.declarePublisher({ stream, publisherRef })
-    publisher.on("publish_confirm", (error, _publishingIds) => {
-      if (error) errors.push(error)
-    })
+    publisher.on("publish_confirm", (error, ids) => publishResponses.push({ error, ids }))
     await rabbit.deleteStream(stream)
 
     await publisher.send(1n, Buffer.from(`test${randomUUID()}`))
 
-    await eventually(() => expect(errors).lengthOf(1))
-    expect(errors).eql([256])
-  }).timeout(10000)
-
-  it("after the server replies with an error, the error callback is invoked with the error as an argument", async () => {
-    const errors: number[] = []
-    const publisher = await connection.declarePublisher({ stream, publisherRef })
-    publisher.on("publish_confirm", (err, _publishingIds) => {
-      if (err) errors.push(err)
-    })
-    await rabbit.deleteStream(stream)
-
-    await publisher.send(1n, Buffer.from(`test${randomUUID()}`))
-
-    await eventually(() => expect(errors).lengthOf(1))
-    expect(errors).eql([256])
+    await eventually(() => expect(publishResponses).eql([{ error: 256, ids: [undefined] }]))
   }).timeout(10000)
 })
