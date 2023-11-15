8926ac8374edb7dfa506ce9251040488c4362ec0
Alberto Barrilá <alberto.barrila@gmail.com>
2023-11-15 16:40:45 +0100

    update example with basic publish/consume

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 const rabbit = require("rabbitmq-stream-js-client")
-const amqplib = require("amqplib")
 const { randomUUID } = require("crypto")
+const { Offset } = require("../dist/requests/subscribe_request")
 
 const rabbitUser = process.env.RABBITMQ_USER || "rabbit"
 const rabbitPassword = process.env.RABBITMQ_PASSWORD || "rabbit"
@@ -22,44 +22,8 @@ async function main() {
 
   await producer.send(Buffer.from("ciao"))
 
-  await createClassicConsumer(streamName)
-
-  await client.deleteStream({ stream: streamName })
-
-  await client.close()
-}
-
-/**
- *
- * @param {string} queueName name of the stream to consume
- * @returns {Promise<void>}
- */
-async function createClassicConsumer(queueName) {
-  return new Promise(async (res, rej) => {
-    try {
-      const conn = await amqplib.connect(`amqp://${rabbitUser}:${rabbitPassword}@localhost`)
-      const ch = await conn.createChannel()
-      await ch.prefetch(1000)
-      await ch.consume(
-        queueName,
-        async (msg) => {
-          if (!msg) {
-            return
-          }
-
-          console.log("ACK", msg.content.toString())
-          ch.ack(msg)
-          if (msg.content.toString() === "ciao") {
-            await ch.close()
-            await conn.close()
-            res()
-          }
-        },
-        { arguments: { "x-stream-offset": "first" } }
-      )
-    } catch (err) {
-      rej(err)
-    }
+  await connection.declareConsumer({ stream: streamName, offset: Offset.first() }, (message) => {
+    console.log(`Received message ${message.content.toString()}`)
   })
 }
 
