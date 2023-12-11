d8cbc17e1051e0d64b88eaff51fa8669130e9bb5
Igor Cappello <igor.cappello@gmail.com>
2023-12-11 17:57:04 +0100

    fix test: add maxFrameSize parameter in PublishRequeest instantiation

--- test/unit/publish_request.test.ts
+++ test/unit/publish_request.test.ts
@@ -5,10 +5,11 @@ import { PublishRequest } from "../../src/requests/publish_request"
 describe("PublishRequest", () => {
   it("Produce a buffer for a long list of messages", () => {
     const publisherId = 1
+    const maxFrameSize = 1024
     const messages = [...Array(100).keys()].map((idx) => {
       return { publishingId: BigInt(idx), message: { content: Buffer.from(randomUUID()) } }
     })
-    const pr = new PublishRequest({ publisherId, messages })
+    const pr = new PublishRequest({ publisherId, messages, maxFrameSize })
 
     const written = pr.toBuffer()
 
