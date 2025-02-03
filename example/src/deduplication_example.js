dda0766e3dd7544b59d09b8e845a7ba5a10f1f85
Diego Ceccacci <diegoceccacci92@gmail.com>
2025-02-03 12:12:17 +0100

    replaced sent with stored

--- example/src/deduplication_example.js
+++ example/src/deduplication_example.js
@@ -29,26 +29,26 @@ async function main() {
   //with deduplication actived, you can send messages without a publishingId; in this case it will be incremental
   await firstDeduplicationPublisher.send(Buffer.from("Test message 1")) //publishingId = 1
   await firstDeduplicationPublisher.send(Buffer.from("Test message 2")) //publishingId = 2
-  //but you can also set a publishingId, note that it must be greater than the last one for the message to be sent
+  //but you can also set a publishingId, note that it must be greater than the last one for the message to be stored
   await firstDeduplicationPublisher.send(Buffer.from("Test message 3"), { publishingId: 3n }) //publishingId = 3
-  //if you choose a publishingId that is less than the last one, the message will not be sent
-  await firstDeduplicationPublisher.send(Buffer.from("Test message 4"), { publishingId: 1n }) //this message won't be sent
+  //if you choose a publishingId that is less than the last one, the message will not be stored
+  await firstDeduplicationPublisher.send(Buffer.from("Test message 4"), { publishingId: 1n }) //this message won't be stored
   await firstDeduplicationPublisher.flush()
   const firstPublisherPublishingId = await firstDeduplicationPublisher.getLastPublishingId()
   await firstDeduplicationPublisher.close()
 
-  console.log(`Publishing id is ${firstPublisherPublishingId} (must be 3)`) //this must be the greatest publishingId sent, 3 in this case
+  console.log(`Publishing id is ${firstPublisherPublishingId} (must be 3)`) //this must be the greatest publishingId, 3 in this case
 
   const secondDeduplicationPublisher = await client.declarePublisher({ stream: streamName, publisherRef: publisherRef })
-  //with the second publisher if we try to send messages with lower publishingId than the last one, they will not be sent
-  await secondDeduplicationPublisher.send(Buffer.from("Test message 5"), { publishingId: 1n }) //won't be sent
-  await secondDeduplicationPublisher.send(Buffer.from("Test message 6"), { publishingId: 2n }) //won't be sent
-  await secondDeduplicationPublisher.send(Buffer.from("Test message 7"), { publishingId: 7n }) //this will be sent since 7 is greater than 3, the last highest publishingId
+  //with the second publisher if we try to send messages with lower publishingId than the last one, they will not be stored
+  await secondDeduplicationPublisher.send(Buffer.from("Test message 5"), { publishingId: 1n }) //won't be stored
+  await secondDeduplicationPublisher.send(Buffer.from("Test message 6"), { publishingId: 2n }) //won't be stored
+  await secondDeduplicationPublisher.send(Buffer.from("Test message 7"), { publishingId: 7n }) //this will be stored since 7 is greater than 3, the last highest publishingId
   await secondDeduplicationPublisher.flush()
   const secondPublisherPublishingId = await secondDeduplicationPublisher.getLastPublishingId()
   await secondDeduplicationPublisher.close()
 
-  console.log(`Publishing id is ${secondPublisherPublishingId} (must be 7)`) //this must be the greatest publishingId sent, 7 in this case
+  console.log(`Publishing id is ${secondPublisherPublishingId} (must be 7)`) //this must be the greatest publishingId, 7 in this case
 
   await client.deleteStream({ stream: streamName })
 
