121910fc599d534b1b975013f1b29a302ed78910
Thomas Shih <tshih113@gmail.com>
2025-05-27 16:48:14 -0600

    refactor: restructure SSL options in TLS client creation

--- test/e2e/connect.test.ts
+++ test/e2e/connect.test.ts
@@ -14,9 +14,12 @@ async function createTlsClient(): Promise<Client> {
     port: 5551,
     mechanism: "EXTERNAL",
     ssl: {
-      ca: await readFile("./tls-gen/basic/result/ca_certificate.pem", "utf8"),
-      cert: await readFile(`./tls-gen/basic/result/client_${firstNode.host}_certificate.pem`, "utf8"),
-      key: await readFile(`./tls-gen/basic/result/client_${firstNode.host}_key.pem`, "utf8"),
+      enabled: true,
+      options: {
+        ca: await readFile("./tls-gen/basic/result/ca_certificate.pem", "utf8"),
+        cert: await readFile(`./tls-gen/basic/result/client_${firstNode.host}_certificate.pem`, "utf8"),
+        key: await readFile(`./tls-gen/basic/result/client_${firstNode.host}_key.pem`, "utf8"),
+      },
     },
     username: "",
     password: "",
