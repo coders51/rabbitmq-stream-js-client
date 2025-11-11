1c5a89792bc6f0744d2cbf115e12a1b63d5d90ec
WhereJuly <where.july@googlemail.com>
2025-11-11 17:18:52 +0300

    Add jsdoc to Code51Exception

--- src/application/Code51Exception.ts
+++ src/application/Code51Exception.ts
@@ -4,6 +4,41 @@ import { ResponseCode } from "../util"
 
 export type TResponseCode = (typeof ResponseCode)[keyof typeof ResponseCode]
 
+/**
+ * Provides distinct domain exception for the package. Contains the optional
+ * RabbitMQ Stream protocol response code for more convenient processing.
+ *
+ * @param message A custom error message.
+ * @param rmqStreamResponseCode The above mentioned response code.
+ *
+ * @see https://github.com/rabbitmq/rabbitmq-server/blob/main/deps/rabbitmq_stream/docs/PROTOCOL.adoc#response-codes
+ *
+ * @example Selectively manage the exception type and react differently.
+ *
+ * ```typescript
+ * let result: any;
+ *
+ * const isRethrowable = (error_: Error) => {
+ *     const isGenericError = error_ instanceof Code51Exception;
+ *     const isNonManagedResponseCode = (error_ as Code51Exception).code !== ResponseCode.NoOffset;
+ *
+ *     return isGenericError && isNonManagedResponseCode;
+ * };
+ *
+ * try {
+ *     result = consumer.queryOffset();
+ *     // ... process result
+ * } catch (error_) {
+ *     if (isRethrowable(error_)) { throw error_; }
+ *
+ *     const error = error_ as Code51Exception;
+ *     if (error.code === ResponseCode.NoOffset) { return null; }
+ *
+ *     return result;
+ * }
+ * ```
+ *
+ */
 export default class Code51Exception extends Error {
   readonly #code?: TResponseCode
 
